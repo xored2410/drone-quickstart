7c3734924443127f33201aacde134be1a0c16bc0
xored2410 <edineminovic1024@gmail.com>
2024-05-19 15:46:50 +0200

    drone local setup

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-export DRONE_GITHUB_CLIENT_ID=
-export DRONE_GITHUB_CLIENT_SECRET=
-export DRONE_GITHUB_ADMIN=
-export DRONE_SERVER_HOST=
+export DRONE_GITHUB_CLIENT_ID="Ov23lieCGKgjr1XVFvO6"
+export DRONE_GITHUB_CLIENT_SECRET="13f09614fadb08a0ea4bf6d13020f5ecc011a21e"
+export DRONE_GITHUB_ADMIN="xored2410"
+export DRONE_SERVER_HOST=fdc4-93-86-167-235.ngrok-free.app
 
 export HOSTNAME=$(hostname)
 export DRONE_RPC_SECRET="$(echo ${HOSTNAME} | openssl dgst -md5 -hex)"
